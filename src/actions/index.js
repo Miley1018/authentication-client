f8adb76ebf593032cd220eb5a3af73c2f8859f7e
Miley Gao <mileygao@Mileys-MacBook-Pro.local>
2017-10-14 13:13:00 -0700

    Rewrite dispatch

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,22 +3,15 @@ import axios from 'axios';
 import {browserHistory} from 'react-router';
 import {AUTH_USER,AUTH_ERROR} from './types';
 
-export function signinUser({email,password}){
-    return function(dispatch){//REDUX-THUNK
-        axios.post(`${ROOT_URL}/signin`,{email,password})
-        .then(response=>{
-            dispatch({type:AUTH_USER});
-            localStorage.setItem('toke',response.data.token);
-           browserHistory.push('/feature'); 
-        }).catch(()=>{
-            dispatch(authError('Bad login info'));
-        });
-    }
-}
-
 export function authError(errorMessage){
     return{
         type:AUTH_ERROR,
         payload:errorMessage
+    }
+}
+
+export function AuthUser(){
+    return {
+        type:AUTH_USER,
     }
 }
--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -3,6 +3,10 @@ import {connect} from 'react-redux';
 import { reduxForm,Field } from 'redux-form'; 
 import * as actions from '../../actions/index';
 
+const ROOT_URL='http://localhost:7777';
+import axios from 'axios';
+import {browserHistory} from 'react-router';
+
 const renderInput = field =>   // Define stateless component to render input and errors
 <div>
   <input {...field.input} type={field.type}/> 
@@ -13,8 +17,18 @@ const renderInput = field =>   // Define stateless component to render input and
 class Signin extends Component{
     handleFormSubmit({email,password}){
         console.log(email,password);
-        this.props.signinUser({email,password});
+        axios.post(`${ROOT_URL}/signin`,{email,password})
+        .then(response=>{
+            this.props.AuthUser();
+            localStorage.setItem('token',response.data.token);
+           browserHistory.push('/feature'); 
+        }).catch(()=>{
+            this.props.authError('Bad login info');
+        });
     }
+
+
+
     renderAlert(){
         if (this.props.errorMessage){
             return (<div>
@@ -55,4 +69,5 @@ export default
 connect(mapStateToProps,actions)(reduxForm({//Creates a decorator with which you use redux-form to connect your form component to Redux. 
     form:'signin'
 })(Signin));//null 就是property， action 就是action);
+
 
